877c5f2c1b7f21921ac084ef79a1ddf19b85ac6c
TheOnlyMrCat <signupforforums5@gmail.com>
2022-07-09 14:13:51 +1000

    feat: Add PolystripDevice and WindowTarget types
    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,170 @@
 pub mod math;
+
+use std::sync::Arc;
+
+pub use wgpu;
+
+pub struct PolystripDevice {
+    pub instance: Arc<wgpu::Instance>,
+    pub adapter: Arc<wgpu::Adapter>,
+    pub device: Arc<wgpu::Device>,
+    pub queue: Arc<wgpu::Queue>,
+}
+
+impl PolystripDevice {
+    /// Create a `PolystripDevice` with default settings read from the environment.
+    ///
+    /// This method uses the initialisation methods in [`wgpu::util`], and therefore reads the same environment variables they
+    /// do. In particular:
+    ///
+    /// - `WGPU_BACKEND`: A comma-separated list of backends for wgpu to use
+    /// - `WGPU_ADAPTER_NAME`: A substring of the name of an adapter for wgpu to use.
+    /// - `WGPU_POWER_PREF`: If `WGPU_ADAPTER_NAME` is unset, "low" or "high" power preference for a default adapter
+    ///
+    /// If these are unset, [`wgpu::Backends::PRIMARY`] and [`wgpu::PowerPreference::LowPower`] will be chosen.
+    ///
+    /// No extra features or limits are requested for the `wgpu::Device`.
+    ///
+    /// This method panics if any part of initialisation fails.
+    pub async fn new_from_env() -> PolystripDevice {
+        let backends = wgpu::util::backend_bits_from_env().unwrap_or(wgpu::Backends::PRIMARY);
+        let instance = wgpu::Instance::new(backends);
+        let adapter = wgpu::util::initialize_adapter_from_env_or_default(&instance, backends, None)
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: Some("Polystrip"),
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+        PolystripDevice {
+            instance: instance.into(),
+            adapter: adapter.into(),
+            device: device.into(),
+            queue: queue.into(),
+        }
+    }
+
+    /// Create a [`WindowTarget`] and a `PolystripDevice` compatible with that target.
+    ///
+    /// See [`Self::new_from_env`] and [`WindowTarget::new`] for more details
+    ///
+    /// # Panics
+    /// Panics on macOS if called from a thread other than the main thread
+    ///
+    /// # Safety
+    /// `handle` must be a valid object to create a [`wgpu::Surface`] upon and must remain valid for the lifetime of the
+    /// returned `WindowTarget`
+    pub async unsafe fn new_from_env_with_window(
+        handle: &impl raw_window_handle::HasRawWindowHandle,
+        (width, height): (u32, u32),
+        present_mode: wgpu::PresentMode,
+    ) -> (PolystripDevice, WindowTarget) {
+        let backends = wgpu::util::backend_bits_from_env().unwrap_or(wgpu::Backends::PRIMARY);
+        let instance = wgpu::Instance::new(backends);
+        let surface = instance.create_surface(&handle);
+        let adapter =
+            wgpu::util::initialize_adapter_from_env_or_default(&instance, backends, Some(&surface))
+                .await
+                .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: Some("Polystrip"),
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+        let device = Arc::new(device);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width,
+            height,
+            present_mode,
+        };
+        surface.configure(&device, &config);
+        let target = WindowTarget::from_wgpu(device.clone(), surface, config);
+        (
+            PolystripDevice {
+                instance: instance.into(),
+                adapter: adapter.into(),
+                device,
+                queue: queue.into(),
+            },
+            target,
+        )
+    }
+}
+
+pub struct WindowTarget {
+    device: Arc<wgpu::Device>,
+    surface: wgpu::Surface,
+    config: wgpu::SurfaceConfiguration,
+}
+
+impl WindowTarget {
+    /// Create a `WindowTarget` from a window, given its size.
+    ///
+    /// # Safety
+    /// `handle` must be a valid object to create a [`wgpu::Surface`] upon and must remain valid for the lifetime of the
+    /// returned `WindowTarget`
+    pub unsafe fn new(
+        device: &PolystripDevice,
+        handle: &impl raw_window_handle::HasRawWindowHandle,
+        (width, height): (u32, u32),
+        present_mode: wgpu::PresentMode,
+    ) -> WindowTarget {
+        let surface = device.instance.create_surface(handle);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width,
+            height,
+            present_mode,
+        };
+        Self::from_wgpu(device.device.clone(), surface, config)
+    }
+
+    /// Create a `WindowTarget` from a pre-initialised [`wgpu::Surface`].
+    ///
+    /// `surface` should already be configured with the passed `config`.
+    pub fn from_wgpu(
+        device: Arc<wgpu::Device>,
+        surface: wgpu::Surface,
+        config: wgpu::SurfaceConfiguration,
+    ) -> WindowTarget {
+        WindowTarget {
+            device,
+            surface,
+            config,
+        }
+    }
+
+    /// Resize and reconfigure the surface.
+    ///
+    /// This should be called in your window's event loop every time the window is resized.
+    pub fn resize(&mut self, (width, height): (u32, u32)) {
+        self.config.width = width;
+        self.config.height = height;
+        self.surface.configure(&self.device, &self.config);
+    }
+
+    /// Change the present mode and reconfigure the surface.
+    ///
+    /// This can be used to implement an interactive VSync option.
+    pub fn set_present_mode(&mut self, present_mode: wgpu::PresentMode) {
+        self.config.present_mode = present_mode;
+        self.surface.configure(&self.device, &self.config);
+    }
+}
+
