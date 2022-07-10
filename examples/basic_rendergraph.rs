7504a509321edbbf3274a54b9b379e05c0497728
TheOnlyMrCat <signupforforums5@gmail.com>
2022-07-10 16:31:22 +1000

    feat: Add basic render graph API

---
+++ examples/basic_rendergraph.rs
@@ -0,0 +1,100 @@
+use pollster::FutureExt;
+use polystrip::{PolystripDevice, RenderGraph, RenderPassTarget, TextureHandle};
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
+fn main() {
+    let el = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Polystrip Basic Rendergraph")
+        .build(&el)
+        .unwrap();
+
+    let (wgpu_device, mut window) = unsafe {
+        PolystripDevice::new_from_env_with_window(
+            &window,
+            window.inner_size().into(),
+            wgpu::PresentMode::AutoVsync,
+        )
+        .block_on()
+    };
+
+    let mut renderer = wgpu_device.create_renderer();
+    let pipeline = create_pipeline(&wgpu_device.device);
+
+    el.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        Event::MainEventsCleared => {
+            let mut graph = RenderGraph::new(&mut renderer);
+            let mut node = graph.add_node();
+            let output_texture = node.add_output_texture(TextureHandle::RENDER_TARGET);
+            let pipeline = node.passthrough_ref(&pipeline);
+            node.build_renderpass(
+                RenderPassTarget::color(
+                    output_texture,
+                    wgpu::Color::BLACK,
+                ),
+                |pass, passthrough, _resources| {
+                    pass.set_pipeline(passthrough.get(pipeline));
+                    pass.draw(0..3, 0..1);
+                },
+            );
+            graph.execute(&mut window);
+        }
+        _ => {}
+    })
+}
+
+const SHADER_SOURCE: &str = "\
+@vertex
+fn vert(@builtin(vertex_index) vertex_index: u32) -> @builtin(position) vec4<f32> {
+    let x = f32(vertex_index >> 1u) * 2.0 - 1.0;
+    let y = f32(vertex_index & 1u) * 2.0 - 1.0;
+    return vec4<f32>(x, y, 0.0, 1.0);
+}
+
+@fragment
+fn frag(@builtin(position) position: vec4<f32>) -> @location(0) vec4<f32> {
+    let hue = acos(dot(position.xy, vec2<f32>(1.0, 1.0)) / (sqrt(2.0) * length(position.xy)));
+    let K = vec4<f32>(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    let p = abs(fract(vec3<f32>(hue, hue, hue) + K.xyz) * 6.0 - K.www) - K.xxx;
+    //TODO: Re-vectorise this clamping when possible?
+    return vec4<f32>(clamp(p.x, 0.0, 1.0), clamp(p.y, 0.0, 1.0), clamp(p.z, 0.0, 1.0), 1.0);
+}
+";
+
+fn create_pipeline(device: &wgpu::Device) -> wgpu::RenderPipeline {
+    let shader_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(SHADER_SOURCE.into()),
+    });
+    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: None,
+        layout: None,
+        vertex: wgpu::VertexState {
+            module: &shader_module,
+            entry_point: "vert",
+            buffers: &[],
+        },
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        fragment: Some(wgpu::FragmentState {
+            module: &shader_module,
+            entry_point: "frag",
+            targets: &[Some(wgpu::ColorTargetState {
+                format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                blend: None,
+                write_mask: wgpu::ColorWrites::ALL,
+            })]
+        }),
+        multiview: None,
+    })
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 pub mod math;
 
-use std::sync::Arc;
-
 pub use wgpu;
+
+use std::hash::{Hash, Hasher};
+use std::marker::PhantomData;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use ahash::{AHashSet, AHasher};
+use either::{Either, Left, Right};
+use indexmap::IndexMap;
 
 pub struct PolystripDevice {
     pub instance: Arc<wgpu::Instance>,
@@ -25,7 +32,13 @@ impl PolystripDevice {
     ///
     /// No extra features or limits are requested for the `wgpu::Device`.
     ///
-    /// This method panics if any part of initialisation fails.
+    /// # Panics
+    /// Panics if any part of initialisation fails, which can happen for any of the following
+    /// reasons:
+    ///
+    /// - Environment variables are set incorrectly, or `WGPU_ADAPTER_NAME` doesn't refer to a
+    /// valid adapter
+    /// - Adapter does not support all features required for wgpu or polystrip to operate
     pub async fn new_from_env() -> PolystripDevice {
         let backends = wgpu::util::backend_bits_from_env().unwrap_or(wgpu::Backends::PRIMARY);
         let instance = wgpu::Instance::new(backends);
@@ -104,6 +117,37 @@ impl PolystripDevice {
             target,
         )
     }
+
+    pub fn create_renderer(&self) -> Renderer {
+        Renderer::new(self.device.clone(), self.queue.clone())
+    }
+}
+
+pub enum OutputTexture {
+    Surface {
+        texture: wgpu::SurfaceTexture,
+        view: wgpu::TextureView,
+    },
+    View(wgpu::TextureView),
+}
+
+impl OutputTexture {
+    pub fn view(&self) -> &wgpu::TextureView {
+        match self {
+            Self::Surface { view, .. } => view,
+            Self::View(view) => view,
+        }
+    }
+
+    pub fn present(self) {
+        if let Self::Surface { texture, .. } = self {
+            texture.present()
+        }
+    }
+}
+
+pub trait RenderTarget {
+    fn get_current_view(&mut self) -> OutputTexture;
 }
 
 pub struct WindowTarget {
@@ -168,3 +212,319 @@ impl WindowTarget {
     }
 }
 
+impl RenderTarget for WindowTarget {
+    fn get_current_view(&mut self) -> OutputTexture {
+        let texture = self.surface.get_current_texture().unwrap();
+        let view = texture.texture.create_view(&Default::default());
+        OutputTexture::Surface { texture, view }
+    }
+}
+
+pub struct Dependency<T>(T);
+
+#[derive(Clone, Copy)]
+pub struct TextureHandle {
+    id: u32,
+}
+
+impl TextureHandle {
+    pub const RENDER_TARGET: TextureHandle = TextureHandle { id: u32::MAX };
+}
+
+pub struct RenderPassTarget {
+    pub color: Vec<RenderPassColorTarget>,
+    pub depth: Option<RenderPassDepthTarget>,
+}
+
+pub struct RenderPassColorTarget {
+    pub handle: Dependency<TextureHandle>,
+    pub resolve: Option<Dependency<TextureHandle>>,
+    pub clear: wgpu::Color,
+}
+
+pub struct RenderPassDepthTarget {
+    pub handle: Dependency<TextureHandle>,
+    pub depth_clear: Option<f32>,
+    pub stencil_clear: Option<u32>,
+}
+
+impl RenderPassTarget {
+    pub fn color(handle: Dependency<TextureHandle>, clear: wgpu::Color) -> RenderPassTarget {
+        RenderPassTarget {
+            color: vec![RenderPassColorTarget {
+                handle,
+                resolve: None,
+                clear,
+            }],
+            depth: None,
+        }
+    }
+
+    pub fn color_with_resolve(handle: Dependency<TextureHandle>, resolve: Dependency<TextureHandle>, clear: wgpu::Color) -> RenderPassTarget {
+        RenderPassTarget {
+            color: vec![RenderPassColorTarget {
+                handle,
+                resolve: Some(resolve),
+                clear,
+            }],
+            depth: None,
+        }
+    }
+}
+
+pub struct Renderer {
+    device: Arc<wgpu::Device>,
+    queue: Arc<wgpu::Queue>,
+    textures: IndexMap<wgpu_types::TextureDescriptor<u64>, (wgpu::Texture, wgpu::TextureView)>,
+}
+
+impl Renderer {
+    pub fn new(device: Arc<wgpu::Device>, queue: Arc<wgpu::Queue>) -> Renderer {
+        Self {
+            device,
+            queue,
+            textures: IndexMap::new(),
+        }
+    }
+}
+
+pub struct RenderPassResources<'pass> {
+    resources: &'pass Renderer,
+    output_texture: &'pass wgpu::TextureView,
+}
+
+impl<'pass> RenderPassResources<'pass> {
+    pub fn get_texture(&self, handle: Dependency<TextureHandle>) -> &'pass wgpu::TextureView {
+        if handle.0.id == u32::MAX {
+            self.output_texture
+        } else {
+            let (_, (_, view)) = self
+                .resources
+                .textures
+                .get_index(handle.0.id as usize)
+                .unwrap();
+            view
+        }
+    }
+}
+
+pub struct RenderGraph<'r, 'node> {
+    renderer: &'r mut Renderer,
+    current_textures: AHashSet<u32>,
+    nodes: Vec<GraphNode<'node>>,
+}
+
+impl<'r, 'node> RenderGraph<'r, 'node> {
+    pub fn new(renderer: &'r mut Renderer) -> RenderGraph<'r, 'node> {
+        RenderGraph {
+            renderer,
+            current_textures: AHashSet::new(),
+            nodes: Vec::new(),
+        }
+    }
+
+    pub fn add_intermediate_texture(
+        &mut self,
+        descriptor: wgpu::TextureDescriptor,
+    ) -> TextureHandle {
+        let mut hash_descriptor = descriptor.map_label(|s| {
+            let mut hasher = AHasher::default();
+            if let Some(s) = s {
+                s.hash(&mut hasher)
+            }
+            hasher.finish()
+        });
+        let id = loop {
+            match self.renderer.textures.get_index_of(&hash_descriptor) {
+                Some(id) => {
+                    if self.current_textures.insert(id as u32) {
+                        break Some(id as u32);
+                    }
+                }
+                None => break None,
+            }
+            hash_descriptor.label += 1;
+        };
+        match id {
+            Some(id) => TextureHandle { id },
+            None => {
+                let texture = self.renderer.device.create_texture(&descriptor);
+                let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+                let (id, old) = self
+                    .renderer
+                    .textures
+                    .insert_full(hash_descriptor, (texture, view));
+                debug_assert!(old.is_none());
+                TextureHandle { id: id as u32 }
+            }
+        }
+    }
+
+    pub fn add_node<'a>(&'a mut self) -> NodeBuilder<'a, 'r, 'node> {
+        NodeBuilder {
+            graph: self,
+            outputs: Vec::new(),
+            passthrough: PassthroughContainer::new(),
+        }
+    }
+
+    pub fn execute(self, target: &mut impl RenderTarget) {
+        let output = target.get_current_view();
+        let resources = RenderPassResources {
+            resources: self.renderer,
+            output_texture: output.view(),
+        };
+
+        let mut encoder = self
+            .renderer
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Polystrip Command Encoder"),
+            });
+
+        for node in self.nodes {
+            match node.render_pass {
+                Some(pass_targets) => {
+                    let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: None,
+                        color_attachments: &pass_targets
+                            .color
+                            .into_iter()
+                            .map(|target| {
+                                Some(wgpu::RenderPassColorAttachment {
+                                    view: resources.get_texture(target.handle),
+                                    resolve_target: target
+                                        .resolve
+                                        .map(|handle| resources.get_texture(handle)),
+                                    ops: wgpu::Operations {
+                                        load: wgpu::LoadOp::Clear(target.clear),
+                                        store: true,
+                                    },
+                                })
+                            })
+                            .collect::<Vec<_>>(),
+                        depth_stencil_attachment: pass_targets.depth.map(|target| {
+                            wgpu::RenderPassDepthStencilAttachment {
+                                view: resources.get_texture(target.handle),
+                                depth_ops: target.depth_clear.map(|value| wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(value),
+                                    store: true,
+                                }),
+                                stencil_ops: target.stencil_clear.map(|value| wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(value),
+                                    store: true,
+                                }),
+                            }
+                        }),
+                    });
+                    (node.exec)(Right(&mut pass), node.passthrough, &resources);
+                }
+                None => {
+                    (node.exec)(Left(&mut encoder), node.passthrough, &resources);
+                }
+            }
+        }
+
+        self.renderer.queue.submit([encoder.finish()]);
+        output.present();
+    }
+}
+
+pub struct NodeBuilder<'a, 'r, 'node> {
+    graph: &'a mut RenderGraph<'r, 'node>,
+    outputs: Vec<TextureHandle>,
+    passthrough: PassthroughContainer<'node>,
+}
+
+impl<'node> NodeBuilder<'_, '_, 'node> {
+    pub fn add_output_texture(&mut self, handle: TextureHandle) -> Dependency<TextureHandle> {
+        self.outputs.push(handle);
+        Dependency(handle)
+    }
+
+    pub fn passthrough_ref<T>(&mut self, data: &'node T) -> PassthroughRef<T> {
+        self.passthrough.insert(data)
+    }
+
+    pub fn build_with_encoder<F>(self, exec: F)
+    where
+        F: for<'b, 'pass> FnOnce(&'b mut wgpu::CommandEncoder, PassthroughContainer<'pass>, &'b RenderPassResources<'pass>)
+            + 'node,
+    {
+        self.graph.nodes.push(GraphNode {
+            outputs: self.outputs,
+            passthrough: self.passthrough,
+            render_pass: None,
+            exec: Box::new(move |encoder, passthrough, res| exec(encoder.unwrap_left(), passthrough, res)),
+        })
+    }
+
+    pub fn build_renderpass<F>(self, pass: RenderPassTarget, exec: F)
+    where
+        F: for<'b, 'pass> FnOnce(&'b mut wgpu::RenderPass<'pass>, PassthroughContainer<'pass>, &'b RenderPassResources<'pass>)
+            + 'node,
+    {
+        self.graph.nodes.push(GraphNode {
+            outputs: self.outputs,
+            passthrough: self.passthrough,
+            render_pass: Some(pass),
+            exec: Box::new(move |encoder, passthrough, res| exec(encoder.unwrap_right(), passthrough, res)),
+        })
+    }
+}
+
+struct GraphNode<'node> {
+    outputs: Vec<TextureHandle>,
+    passthrough: PassthroughContainer<'node>,
+    render_pass: Option<RenderPassTarget>,
+    exec: Box<
+        dyn for<'b, 'pass> FnOnce(
+                Either<&'b mut wgpu::CommandEncoder, &'b mut wgpu::RenderPass<'pass>>,
+                PassthroughContainer<'pass>,
+                &'b RenderPassResources<'pass>,
+            ) + 'node,
+    >,
+}
+
+pub struct PassthroughContainer<'a> {
+    id: usize,
+    data: Vec<*const ()>,
+    _marker: PhantomData<&'a ()>
+}
+
+pub struct PassthroughRef<T> {
+    container_id: usize,
+    data_idx: usize,
+    _marker: PhantomData<*const T>, // For variance
+}
+
+impl<'a> PassthroughContainer<'a> {
+    fn new() -> Self {
+        static ID: AtomicUsize = AtomicUsize::new(0);
+        Self {
+            id: ID.fetch_add(1, Ordering::Relaxed),
+            data: vec![],
+            _marker: PhantomData,
+        }
+    }
+
+    fn insert<T>(&mut self, data: &'a T) -> PassthroughRef<T> {
+        self.data.push(data as *const T as *const ());
+        PassthroughRef {
+            container_id: self.id,
+            data_idx: self.data.len() - 1,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'pass> PassthroughContainer<'pass> {
+    pub fn get<T>(&self, handle: PassthroughRef<T>) -> &'pass T {
+        assert_eq!(self.id, handle.container_id);
+        unsafe {
+            // SAFETY: `handle` cannot have been created except by us, and is type-tagged to be
+            // something covariant to the original type stored.
+            &*(self.data[handle.data_idx] as *const T)
+        }
+    }
+}
